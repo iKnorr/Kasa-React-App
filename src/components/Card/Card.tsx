6db5f6a3dfe4da0f1b269f67a52be35266e96078
Ingo Knorr <ingoknorr@gmail.com>
2023-07-20 22:05:03 +0200

    changes to media queries

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -11,7 +11,6 @@ const Card = ({ title, src, id }: CardProps) => {
   return (
     <Link to={`/accommodation/${id}`} className={styles.link}>
       <div className={styles.wrapper}>
-        {/* <img src={src} alt={title} /> */}
         <p>{title}</p>
       </div>
     </Link>
